31242e56b59ee8a1b5b34d98f80b3351fcd04419
ksdfs1 <mbjo001101@gmail.com>
2022-03-12 17:45:52 +0900

    added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,11 @@
+
+public class MenuManager {
+	
+	public static void main(String[] args) {
+		System.out.println("Add Schedules");
+		System.out.println("Delete Schedules");
+		System.out.println("Edit Schedules");
+		System.out.println("View Schedules");
+		System.out.println("Exit");
+	}
+}
